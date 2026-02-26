a4ef253082c92aa71ced0241c0771fb639fe17ce
Kewen Santiago <kewensl197@gmail.com>
2026-02-26 15:20:49 -0300

    Chore: gitignore add

--- src/main/java/dev/Java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/Java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -2,6 +2,9 @@ package dev.Java10x.CadastroDeNinjas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.h2.tools.Server;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class CadastroDeNinjasApplication {
@@ -10,4 +13,10 @@ public class CadastroDeNinjasApplication {
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
 	}
 
+	// Força a inicialização do Console do H2 em uma porta separada
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseaServer() throws SQLException {
+		return Server.createWebServer(
+				"-web", "-webAllowOthers", "-webPort", "8082");
+	}
 }
---
+++ src/main/java/dev/Java10x/CadastroDeNinjas/TesteController.java
@@ -0,0 +1,13 @@
+package dev.Java10x.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TesteController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "Servidor está online e mapeando rotas com sucesso!";
+    }
+}
