53864b448baeb5f4749fc6c39e6b00d055882795
Kewen Santiago <kewensl197@gmail.com>
2026-02-28 18:05:48 -0300

    feat: update controller

--- src/main/java/dev/Java10x/CadastroDeninjas/Ninjas/NinjaController.java
+++ src/main/java/dev/Java10x/CadastroDeninjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package dev.Java10x.CadastroDeninjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,38 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas() {
         return "Essa é minha primeira mensagem nessa rota";
+    }
+
+
+    //CRUD
+//Adicionar ninja (CREATE)
+    @PostMapping("/criar")
+    public String criarNinja() {
+        return "Ninja criado";
+    }
+//Mostrar todos os ninjas (READ)
+
+    @GetMapping("/todos")
+    public String mostrarTodosOsNinjas() {
+        return "Mostrar Ninja";
+    }
+
+//Procurar ninja por ID (READ)
+
+    @GetMapping("/todosIDF")
+    public String mostrarTodosOsNinjasPorId() {
+        return "Mostar ninja por ID";
+    }
+
+    //Alterar dados dos ninjas (UPDATE)
+    @PutMapping("/alterar")
+    public String alterarNinjaPorId() {
+        return "Alterar ninja por ID";
+    }
+
+    //Deletar ninja (DELETE)
+    @DeleteMapping("/deletarID")
+    public String deletarNinjaPorId() {
+        return "Deletar ninja por ID";
     }
 }
--- src/main/resources/db/migrations/V2__Add_rank_tb_cadastro.sql
+++ src/main/resources/db/migrations/V2__Add_rank_tb_cadastro.sql
@@ -1,4 +1,4 @@
 --V2: Migrations para adicionar a coluna e RANK na tavela de cadastro
 
 ALTER TABLE tb_cadastro
-    ADD COLUMN rank VARCHAR(255);
+    ADD COLUMN rank_ninja VARCHAR(255);
